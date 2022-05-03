76db74ca55f6b1bd5e6e9dbf23fadf58dbb43edd
James Jean-Marie <jeanmariejames51@gmail.com>
2022-05-03 22:03:11 +0200

    Ajoutez des fichiers projet.

---
+++ Learn/Character.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn
+{
+    public class Character
+    {
+        public string Name;
+        public int Health;
+        public int Strength;
+        public int Defense;
+
+        public Character(string name, int health, int strength, int defense)
+        {
+            this.Name = name;
+            this.Health = health;
+            this.Strength = strength;
+            this.Defense = defense;
+        }
+
+        public bool IsAlive()
+        {
+            if (this.Health <= 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public int Attack(Character target)
+        {
+            var rand = new Random();
+            int damage = rand.Next((this.Strength/2),this.Strength);
+            int tHealth = target.Health;
+            if (target.Defense <= this.Strength)
+            {
+                target.Health -= damage;
+            }
+            else
+            {
+                this.Health -= rand.Next((this.Defense/2), (target.Defense));
+            }
+            return tHealth - target.Health;
+        }
+    }
+}
---
+++ Learn/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Learn
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            Character character = new Character("Jagle", 100, 10, 7);
+            Character target = new Character("Nooskya", 150, 7, 10);
+            int round = 1;
+            int cDmg = 0;
+            int tDmg = 0;
+            while (character.IsAlive() == true && target.IsAlive() == true)
+            {
+                round += 1;
+                Console.WriteLine("Round " + round);
+                cDmg = character.Attack(target);
+                tDmg = target.Attack(character);
+
+                Console.WriteLine("{0} attack {1} for {2} damage, only {3} hp left\n", character.Name, target.Name, tDmg, character.Health);
+                Console.WriteLine("{0} attack {1} for {2} damage, only {3} hp left\n", target.Name, character.Name, cDmg, target.Health);
+                
+            }
+            if(character.IsAlive() == true){
+
+            Console.WriteLine("{0} is dead {1} killed him and still have {2} hp\n", target.Name, character.Name, character.Health);
+                
+            } else if(character.IsAlive() == false && target.IsAlive() == false)
+            {
+                Console.WriteLine("Both {0} and {1} are dead, there is no winner", character.Name, target.Name);
+            }
+            else
+            {
+                Console.WriteLine("{0} is dead {1} killed him and still have {2} hp\n", character.Name, target.Name, target.Health);
+            }
+        }
+    }
+}
